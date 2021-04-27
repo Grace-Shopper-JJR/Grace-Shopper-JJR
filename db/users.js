ec3eb802bb1d1c4557f1a40d645962e9664d3949
ElegantMrS <josephjumber@gmail.com>
2021-04-27 19:43:25 -0400

    Organized and caught up

---
+++ db/users.js
@@ -0,0 +1,116 @@
+const client = require('./client_db');
+const { getPreferencesByUserId } = require('./userprefs');
+
+async function createUser({ username, password, firstname, lastname }) {
+
+    try {
+        const { rows: [ user ] } = await client.query(`
+                INSERT INTO users(username, password, firstname, lastname) 
+                VALUES($1, $2, $3, $4) 
+                ON CONFLICT (username) DO NOTHING 
+                RETURNING *;
+            `, [ username, password, firstname, lastname ]);
+
+        return user;
+    } catch (error) {
+        throw error;
+    };
+
+};
+
+async function updateUser(userId, fields = {}) {
+
+    const setString = Object.keys(fields).map(
+        (key, index) => `"${ key }"=$${ index + 1 }`
+    ).join(', ');
+
+    if (setString.length === 0) {
+        return;
+    };
+
+    try {
+        const { rows: [ user ] } = await client.query(`
+            UPDATE users
+            SET ${ setString }
+            WHERE user_id=${ userId }
+            RETURNING *;
+        `, Object.values(fields));
+
+        return user;
+    } catch (error) {
+        throw error;
+    };
+
+};
+
+async function getUserByUserId(userId) {
+
+    try {
+        const { rows: [ user ] } = await client.query(`
+            SELECT user_id, username, firstname, lastname, active
+            FROM users
+            WHERE user_id=$1;
+        `, [ userId ]);
+
+        const { rows: [ userPreferences ] } = await client.query(`
+            SELECT *
+            FROM userPreferences
+            WHERE "userId"=$1;
+        `, [ userId ]);
+
+        user.userPreferences = userPreferences
+        
+        return user;
+    } catch (error) {
+        throw error;
+    };
+
+};
+
+async function getUserByUsername(username) {
+
+    try {
+        const { rows: [ user ] } = await client.query(`
+            SELECT *
+            FROM users
+            WHERE username=$1
+        `, [ username ]);
+
+        return user;
+    } catch (error) {
+        throw error;
+    };
+
+};
+
+async function getAllUsers() {
+
+    console.log("IN GETALLUSERS")
+    try {
+        const { rows: userIds } = await client.query(`
+            SELECT user_id, username, firstname, lastname, active
+            FROM users;
+    `);
+
+    console.log(">>>USERIDS<<<", userIds);
+
+    const users = await Promise.all(userIds.map(
+        user => getUserByUserId(user.user_id)
+    ));
+
+    console.log(">>>USERS<<<", users);
+
+        return users;
+    } catch (error) {
+        throw error;
+    };
+
+};
+
+module.exports = {
+    createUser,
+    updateUser,
+    getUserByUserId,
+    getUserByUsername,
+    getAllUsers,
+};
